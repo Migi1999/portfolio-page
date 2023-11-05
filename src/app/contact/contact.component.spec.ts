bf093342c45d5e480d3c772190a2d5ca3dc4cbd9
mjs99 <mikael.steiner@students.bfh.ch>
2023-11-05 14:11:13 +0100

    some little changes

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,21 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContactComponent } from './contact.component';
 
-describe('ContactComponent', () => {
-  let component: ContactComponent;
-  let fixture: ComponentFixture<ContactComponent>;
+document.getElementById("contact-form")?.addEventListener("submit", function (event) {
+  event.preventDefault(); // Verhindert das Standardverhalten des Formulars
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ContactComponent]
-    });
-    fixture = TestBed.createComponent(ContactComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // Formulardaten sammeln
+  const emailInput = document.getElementById("email") as HTMLInputElement;
+  const messageInput = document.getElementById("message") as HTMLTextAreaElement;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  const email = emailInput.value;
+  const message = messageInput.value;
+
+  // Validierung der E-Mail-Adresse
+  if (!isValidEmail(email)) {
+    alert("Bitte geben Sie eine gültige E-Mail-Adresse ein.");
+    return;
+  }
+
+  // Validierung der Nachricht
+  if (!email || !message) {
+    alert("Bitte füllen Sie alle Felder aus.");
+    return;
+  }
+
+  // Hier kannst du AJAX verwenden, um die Daten an den Server zu senden
+  // ...
+
+  // Rest des Codes bleibt unverändert
 });
+
+// Hilfsfunktion zur Überprüfung der E-Mail-Adresse
+function isValidEmail(email: string): boolean {
+  const emailRegex: RegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
